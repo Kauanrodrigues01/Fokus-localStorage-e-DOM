6a8ef15d04846e1dd08addc5f85f0ac162dfe30a
Kauanrodrigues01 <kauanrl09@gmail.com>
2024-07-05 14:37:19 -0300

    concluido, por mim

--- script/script-crud.js
+++ script/script-crud.js
@@ -2,9 +2,18 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa =  document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const containerTarefas = document.querySelector('.app__section-task-list')
+const paragrafoTarefaEmAndamento = document.querySelector('.app__section-active-task-description')
+const dropdownMenuOpcoes = document.querySelector('.app__section-task-header__ul')
+const bntLimparTarefasConcluidas = dropdownMenuOpcoes.children[0]
+const bntLimparTarefas = dropdownMenuOpcoes.children[1]
 
 // Pegando informações da localStore
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
+
+const atualizarTarefas = () => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
@@ -13,13 +22,27 @@ btnAdicionarTarefa.addEventListener('click', () => {
 
 formAdicionarTarefa.addEventListener('submit', (evt) => {
     evt.preventDefault()
-    const tarefa = {
-        descricao: textarea.value
+    if (textarea.value.length < 5){
+        alert('A descrição da tarefa tem que ter no mínimo 5 caracteres')
+    } else{
+        const tarefa = {
+            descricao: textarea.value
+        }
+        tarefas.push(tarefa)
+        atualizarTarefas()
+        textarea.value = ''
+        formAdicionarTarefa.classList.add('hidden')
+        carregarTarefas() // Após adicionar uma tarefa, carrega novamente a lista de tarefas
     }
-    tarefas.push(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-    carregarTarefas() // Após adicionar uma tarefa, carrega novamente a lista de tarefas
-    textarea.value = '' // Limpa o textarea após adicionar a tarefa
+    
+})
+
+bntLimparTarefas.addEventListener('click', () => {
+    limparTodasTarefas()   
+})
+
+bntLimparTarefasConcluidas.addEventListener('click', () => {
+    limparTarefasConcluidas();   
 })
 
 const carregarTarefas = () => {
@@ -43,6 +66,22 @@ const carregarTarefas = () => {
 
         const botao = document.createElement('button')
         botao.classList.add('app_button-edit')
+
+        botao.onclick = () => {
+            // debugger
+            const novaDescricao= prompt('Qual é o novo nome da tarefa?')
+            if (novaDescricao == null){
+                console.log('é nulo')
+                return
+            } else if (novaDescricao.length < 5 ){
+                alert('A descrição da tarefa deve ter no mínimo 5 caracteres')
+            }  else{
+                paragrafo.textContent = novaDescricao
+                tarefa.descricao = novaDescricao
+                atualizarTarefas()
+            }
+        }
+
         const imagemBt = document.createElement('img')
         imagemBt.setAttribute('src', './imagens/edit.png')
         botao.appendChild(imagemBt)
@@ -51,9 +90,61 @@ const carregarTarefas = () => {
         li.appendChild(paragrafo)
         li.appendChild(botao)
 
+        if (tarefa.concluida){
+            li.classList.add('app__section-task-list-item-complete')
+            botao.setAttribute('disabled', true)
+        }
+        else{
+            li.onclick = () => {
+                // debugger
+
+                const lis = [...containerTarefas.children]
+                lis.forEach((el) => {
+                    el.classList.remove('app__section-task-list-item-active')
+                })
+
+
+                if(tarefaSelecionada == tarefa){
+                    paragrafoTarefaEmAndamento.textContent = ''
+                    tarefaSelecionada = null
+                    return
+                }
+
+                tarefaSelecionada = [tarefa, li]
+                paragrafoTarefaEmAndamento.textContent = tarefa.descricao
+                li.classList.add('app__section-task-list-item-active')
+            }
+        }
+        
+
         containerTarefas.appendChild(li) // Adiciona o item na lista HTML
     })
 }
 
 // Carrega as tarefas ao iniciar o script
 carregarTarefas()
+
+document.addEventListener('FocoFinalizado', () => {
+    // console.log(tarefaSelecionada[0])
+    // console.log(tarefaSelecionada[1])
+    if (tarefaSelecionada[0] && tarefaSelecionada[1]){
+        tarefaSelecionada[1].classList.remove('app__section-task-list-item-active')
+        tarefaSelecionada[1].classList.add('app__section-task-list-item-complete')
+        tarefaSelecionada[1].querySelector('button').setAttribute('disabled', true)
+
+        tarefaSelecionada[0].concluida = true
+        atualizarTarefas()
+    }
+})
+
+const limparTodasTarefas = () => {
+    tarefas = []
+    atualizarTarefas()
+    carregarTarefas()
+}
+
+const limparTarefasConcluidas = () => {
+    tarefas = tarefas.filter((tarefa) => !tarefa.concluida)
+    atualizarTarefas()
+    carregarTarefas()
+}
--- script/script.js
+++ script/script.js
@@ -19,7 +19,7 @@ musica.loop = true
 const startPauseBt = document.querySelector('#start-pause')
 const audioPlay = new Audio('./sons/play.wav')
 const audioPausa = new Audio('./sons/pause.mp3')
-const audioBeep = new Audio('./sons/beep.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 const imgBtComecar = startPauseBt.children[0]
 const ComecarPausarBt = startPauseBt.children[1]
 const tempoNaTela = document.querySelector('#timer')
@@ -39,28 +39,28 @@ focoBt.addEventListener('click', () => {
     imgBtComecar.setAttribute('src', './imagens/play_arrow.png')
     ComecarPausarBt.textContent = 'Começar'
     pausar()
-    tempoDecorridoEmSegundos = 1500
-    alterarModoDeTempo('foco',  focoBt, '25:00')
+    tempoDecorridoEmSegundos = 5
+    alterarModoDeTempo('foco',  focoBt)
 })
 
 curtoBt.addEventListener('click', () => {
     imgBtComecar.setAttribute('src', './imagens/play_arrow.png')
     ComecarPausarBt.textContent = 'Começar'
     pausar()
-    tempoDecorridoEmSegundos = 300
-    alterarModoDeTempo('descanso-curto', curtoBt, '05:00')
+    tempoDecorridoEmSegundos = 5
+    alterarModoDeTempo('descanso-curto', curtoBt)
 })
 
 longoBt.addEventListener('click', () => {
     imgBtComecar.setAttribute('src', './imagens/play_arrow.png')
     ComecarPausarBt.textContent = 'Começar'
     pausar()
-    tempoDecorridoEmSegundos = 900
-    alterarModoDeTempo('descanso-longo', longoBt, '15:00')
+    tempoDecorridoEmSegundos = 5
+    alterarModoDeTempo('descanso-longo', longoBt)
 })
 
 // Função para alternar o modo de descanso
-const alterarModoDeTempo = (contexto, el, temporizador) => {
+const alterarModoDeTempo = (contexto, el) => {
     mostrarTempo()
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
@@ -70,8 +70,6 @@ const alterarModoDeTempo = (contexto, el, temporizador) => {
     longoBt.classList.remove('active')
 
     el.classList.add('active')
-
-    displayTempo.innerHTML = temporizador
 
     switch (contexto) {
         case "foco":
@@ -100,8 +98,29 @@ const contagemRegressiva = ()=>{
         console.log('temporizador: ' + tempoDecorridoEmSegundos)
         mostrarTempo()
     } else{
-        audioBeep.play()
+        audioTempoFinalizado.play()
         alert('Tempo finalizado!')
+
+        imgBtComecar.setAttribute('src', './imagens/play_arrow.png')
+        ComecarPausarBt.textContent = 'Começar'
+        // debugger
+        if (html.dataset.contexto == 'foco'){
+            tempoDecorridoEmSegundos = 1500
+            mostrarTempo()
+        } else if (html.dataset.contexto == 'descanso-curto'){
+            tempoDecorridoEmSegundos = 300
+            mostrarTempo()
+        } else{
+            tempoDecorridoEmSegundos = 900
+            mostrarTempo()
+        }
+
+        const focoAtico = html.dataset.contexto == 'foco'
+        if (focoAtico){
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
+
         pausar()
         return
     }
